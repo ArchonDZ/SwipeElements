544097181b5184f6b8a3ef4b3d5caa9709a29d60
Dmitry Zinovsky <dima_zinovskiy@mail.ru>
2025-12-17 20:13:04 +0300

    Updated ProjectInstaller: input system
    Updated ProjectInstaller: input system

--- Assets/Project/Scripts/Installers/ProjectInstaller.cs
+++ Assets/Project/Scripts/Installers/ProjectInstaller.cs
@@ -8,12 +8,14 @@ namespace Elements.Installers
     public class ProjectInstaller : MonoInstaller
     {
         [SerializeField] private ProjectSettingsConfig projectSettingsConfig;
+        [SerializeField] private InputSystem inputSystem;
         [SerializeField] private LevelSystem levelSystem;
 
         public override void InstallBindings()
         {
             Container.Bind<ProjectSettingsConfig>().FromInstance(projectSettingsConfig).AsSingle().NonLazy();
             Container.Bind<SaveSystem>().AsSingle().NonLazy();
+            Container.BindInterfacesAndSelfTo<InputSystem>().FromInstance(inputSystem).AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<LevelSystem>().FromInstance(levelSystem).AsSingle().NonLazy();
         }
     }
